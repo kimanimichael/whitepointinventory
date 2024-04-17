6ec8119423d98329b4c18124145695f519f8eb94
MK <mikalkimmani@gmail.com>
2024-04-17 21:18:01 +0300

    (master): Init commit -json repsonses

---
+++ json.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Server side error", msg)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Unable to marshal JSON from paload err: %v", err)
+	}
+	w.Header().Add("Content-Type","application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
