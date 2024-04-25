1f70907b5e796e13199d79b9bcd8b1e934861636
MK <muchunumike@gmail.com>
2024-04-25 21:29:55 +0300

    f(auth): Change authentication to use email instead of name

--- auth/auth.go
+++ auth/auth.go
@@ -8,9 +8,9 @@ import (
 
 /*
 Get Password from http header
-Authorization Password {Insert Password here}:Name {Insert Name here}
+Authorization Password {Insert Password here}:Email {Insert Email here}
 */
-func GetPasswordAndName(headers http.Header)(string, string, error) {
+func GetPasswordAndEmail(headers http.Header)(string, string, error) {
 	val := headers.Get("Authorization")
 	if val == "" {
 		return "", "",  errors.New("no authentication info found")
@@ -18,26 +18,30 @@ func GetPasswordAndName(headers http.Header)(string, string, error) {
 
 	vals := strings.Split(val, ":")
 	if len(vals) != 2 {
-		return "", "", errors.New("malformed authentication info")
+		return "", "", errors.New("malformed authentication info:Wrong auth format")
 	}
 
 	valPassword := vals[0]
 	valsPassword := strings.Split(valPassword, " ")
 	if len(valsPassword) != 2 {
-		return "", "", errors.New("malformed authentication info")
+		return "", "", errors.New("malformed authentication info:Password info wrong format")
 	}
 	if valsPassword[0] != "Password" {
-		return "", "", errors.New("malformed authentication info")
+		return "", "", errors.New("malformed authentication info:Password phrase missing")
 	}
 
 
 	valName := vals[1]
 	valsName := strings.Split(valName, " ")
 	if len(valsName) < 2 {
-		return "", "", errors.New("malformed authentication info")
+		return "", "", errors.New("malformed authentication info:Email info wring format")
 	}
-	if valsName[0] != "Name" {
-		return "", "", errors.New("malformed authentication info")
+	if valsName[0] != "Email" {
+		return "", "", errors.New("malformed authentication info:Email phrase missing")
+	}
+
+	if !strings.Contains(valsName[1], "@") {
+		return "", "", errors.New("malformed authentication info:Bad email entered")
 	}
 	
 	
--- middleware_auth.go
+++ middleware_auth.go
@@ -12,15 +12,15 @@ type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
 func (apiCfg *apiConfig) middlewareAuth (handler authedHandler) http.HandlerFunc{
 	return func (w http.ResponseWriter, r *http.Request)  {
-		password, name, err := auth.GetPasswordAndName(r.Header)
+		password, email, err := auth.GetPasswordAndEmail(r.Header)
 		if err != nil {
 			respondWithError(w, 400, fmt.Sprintf("Auth error: %v", err))
 			return
 		}
 
-		user, err := apiCfg.DB.GetUserByPasswordAndUserName(r.Context(), database.GetUserByPasswordAndUserNameParams{
+		user, err := apiCfg.DB.GetUserByPasswordAndEmail(r.Context(), database.GetUserByPasswordAndEmailParams{
 			Password: password,
-			Name: name,
+			Email: email,
 		})
 		if err != nil {
 			respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
