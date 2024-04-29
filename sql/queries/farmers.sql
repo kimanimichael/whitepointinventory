c356986a18ccd480c0c45dca51b7b5d8b3d16573
MK <muchunumike@gmail.com>
2024-04-29 22:59:32 +0300

    f(purchases_crud): Create sql for increase and decrease chicken and cash owed columns functions using sqlc

--- sql/queries/farmers.sql
+++ sql/queries/farmers.sql
@@ -8,3 +8,24 @@ SELECT * FROM farmers where name = $1;
 
 -- name: DeleteFarmers :exec
 DELETE FROM farmers where id = $1;
+
+-- name: IncreaseChickenOwed :exec
+UPDATE farmers
+SET chicken_balance = COALESCE(chicken_balance, 0) + $1
+WHERE farmers.id = $2;
+
+-- name: IncreaseCashOwed :exec
+UPDATE farmers
+SET cash_balance = COALESCE(cash_balance, 0) + ($1)
+WHERE farmers.id = $2;
+
+-- name: DecreaseChickenOwed :exec
+UPDATE farmers
+SET chicken_balance = COALESCE(chicken_balance, 0) - $1
+WHERE farmers.id = $2;
+
+-- name: DecreaseCashOwed :exec
+UPDATE farmers
+SET cash_balance = COALESCE(cash_balance, 0) - ($1)
+WHERE farmers.id = $2;
+
