354f58ae4909e5d31b833f561e94e4e4d684736e
MK <muchunumike@gmail.com>
2024-05-01 19:01:40 +0300

    f(payments_crud): Add sql for generating creating, getting and deleting payments functions using sqlc

---
+++ sql/queries/payments.sql
@@ -0,0 +1,14 @@
+-- name: CreatePayment :one
+INSERT INTO payments (id, created_at, updated_at, cash_paid, price_per_chicken_paid, user_id, farmer_id)
+VALUES($1, $2, $3, $4, $5, $6, $7)
+RETURNING *;
+
+-- name: GetPayments :many
+SELECT * FROM payments;
+
+-- name: GetPaymentByID :one
+SELECT * FROM payments
+WHERE id = $1;
+
+-- name: DeletePayments :exec
+DELETE FROM payments WHERE id = $1;
