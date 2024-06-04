2651e8f88b414c95d07e956ad7c7007745f9c822
MK <muchunumike@gmail.com>
2024-06-04 08:35:22 +0300

    f(serve_files_static): Add function to test route

--- main_test.go
+++ main_test.go
@@ -23,3 +23,19 @@ func TestHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v wanted%v", status, http.StatusOK)
 	}
 }
+
+func TestRouter(t *testing.T) {
+	r := newTestRouter("/healthz", handlerHealth)
+
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL+"/healthz")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be %v Got %v", http.StatusOK, resp.StatusCode)
+	}
+}
