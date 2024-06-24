3e4e9050eacc125ae3f32ba6f67a18788ae52ed9
MK <muchunumike@gmail.com>
2024-06-25 01:39:23 +0300

    feat(use_api_key_for_auth): Get user from APIKey

--- middleware_auth.go
+++ middleware_auth.go
@@ -11,16 +11,13 @@ type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
 func (apiCfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		password, email, err := auth.GetPasswordAndEmail(r.Header)
+		APIKey, err := auth.GetAPIKey(r.Header)
 		if err != nil {
 			respondWithError(w, 400, fmt.Sprintf("Auth error: %v", err))
 			return
 		}
+		user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), APIKey)
 
-		user, err := apiCfg.DB.GetUserByPasswordAndEmail(r.Context(), database.GetUserByPasswordAndEmailParams{
-			Password: password,
-			Email:    email,
-		})
 		if err != nil {
 			respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 			return
