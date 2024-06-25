c57915fe3d499a33b392aed349cf028e6d205780
MK <muchunumike@gmail.com>
2024-06-25 23:01:37 +0300

    feat(create_custom_payment): Create custom payment struct

--- models.go
+++ models.go
@@ -18,6 +18,18 @@ type Purchase struct {
 	FarmerName      string    `json:"farmer_name"`
 }
 
+type Payment struct {
+	ID                  uuid.UUID `json:"id"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
+	CashPaid            int32     `json:"cash_paid"`
+	PricePerChickenPaid int32     `json:"price_per_chicken_paid"`
+	UserID              uuid.UUID `json:"user_id"`
+	FarmerID            uuid.UUID `json:"farmer_id"`
+	UserName            string    `json:"user_name"`
+	FarmerName          string    `json:"farmer_name"`
+}
+
 func databasePurchaseToPurchase(dbPurchase database.Purchase) Purchase {
 	return Purchase{
 		ID:              dbPurchase.ID,
@@ -36,4 +48,24 @@ func databasePurchasesToPurchases(dbPurchases []database.Purchase) []Purchase {
 		purchases = append(purchases, databasePurchaseToPurchase(dbPurchase))
 	}
 	return purchases
+}
+
+func databasePaymentToPayment(dbPayment database.Payment) Payment {
+	return Payment{
+		ID:                  dbPayment.ID,
+		CreatedAt:           dbPayment.CreatedAt,
+		UpdatedAt:           dbPayment.UpdatedAt,
+		CashPaid:            dbPayment.CashPaid,
+		PricePerChickenPaid: dbPayment.PricePerChickenPaid,
+		UserID:              dbPayment.UserID,
+		FarmerID:            dbPayment.FarmerID,
+	}
+}
+
+func databasePaymentsToPayments(dbPayments []database.Payment) []Payment {
+	payments := []Payment{}
+	for _, dbPayment := range dbPayments {
+		payments = append(payments, databasePaymentToPayment(dbPayment))
+	}
+	return payments
 }
