70a594606c51732554b67a9ce1473bb1652e9a50
MK <muchunumike@gmail.com>
2024-08-11 02:10:55 +0300

    feat(standard_layout): Refactor purchases to internal/

--- internal/purchases/purchases.go
+++ internal/purchases/purchases.go
@@ -1,9 +1,11 @@
-package main
+package purchases
 
 import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/mike-kimani/whitepointinventory/internal/models"
+	"github.com/mike-kimani/whitepointinventory/pkg/jsonresponses"
 	"net/http"
 	"time"
 
@@ -24,40 +26,44 @@ const MinChickenPrice = 100
 // MaxChickenPrice prevents entry errors e.g. 4500 instead of 450 for price
 const MaxChickenPrice = 1000
 
-func (apiCfg *apiConfig) handerCreatePurchases(w http.ResponseWriter, r *http.Request, user database.User) {
+type ApiConfig struct {
+	DB *database.Queries
+}
+
+func (apiCfg *ApiConfig) HandlerCreatePurchases(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
 		Chicken    int32  `json:"chicken_no"`
 		Price      int32  `json:"chicken_price"`
 		FarmerName string `json:"farmer_name"`
 	}
 
-	chicken_bought := sql.NullFloat64{}
-	cash_balance := sql.NullInt32{}
+	chickenBought := sql.NullFloat64{}
+	cashBalance := sql.NullInt32{}
 
 	params := parameters{}
 
 	decode := json.NewDecoder(r.Body)
 	err := decode.Decode(&params)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error decoding json: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Error decoding jsonresponses: %v", err))
 		return
 	}
 	if params.FarmerName == "" {
-		respondWithError(w, 400, "Farmer name is required")
+		jsonresponses.RespondWithError(w, 400, "Farmer name is required")
 		return
 	}
 	if params.Chicken < MinChickenNumber || params.Chicken > MaxChickenNumber {
-		respondWithError(w, 400, fmt.Sprintf("Chicken number must be within %d and %d", MinChickenNumber, MaxChickenNumber))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Chicken number must be within %d and %d", MinChickenNumber, MaxChickenNumber))
 		return
 	}
 
 	if params.Price < MinChickenPrice || params.Price > MaxChickenPrice {
-		respondWithError(w, 400, fmt.Sprintf("Chicken price must be within %d and %d", MinChickenPrice, MaxChickenPrice))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Chicken price must be within %d and %d", MinChickenPrice, MaxChickenPrice))
 		return
 	}
 	farmer, err := apiCfg.DB.GetFarmerByName(r.Context(), params.FarmerName)
 	if err != nil {
-		respondWithError(w, 500, fmt.Sprintf("Error getting farmer: %v", err))
+		jsonresponses.RespondWithError(w, 500, fmt.Sprintf("Error getting farmer: %v", err))
 		return
 	}
 
@@ -72,68 +78,68 @@ func (apiCfg *apiConfig) handerCreatePurchases(w http.ResponseWriter, r *http.Re
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error creating purchase: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Error creating purchase: %v", err))
 		return
 	}
 
-	chicken_bought.Float64 = float64(params.Chicken)
-	chicken_bought.Valid = true
+	chickenBought.Float64 = float64(params.Chicken)
+	chickenBought.Valid = true
 
-	cash_balance.Int32 = params.Chicken * params.Price
-	cash_balance.Valid = true
+	cashBalance.Int32 = params.Chicken * params.Price
+	cashBalance.Valid = true
 
 	err = apiCfg.DB.IncreaseChickenOwed(r.Context(), database.IncreaseChickenOwedParams{
-		ChickenBalance: chicken_bought,
+		ChickenBalance: chickenBought,
 		ID:             farmer.ID,
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error adding chicken owed to farmer database: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Error adding chicken owed to farmer database: %v", err))
 		return
 	}
 
 	err = apiCfg.DB.IncreaseCashOwed(r.Context(), database.IncreaseCashOwedParams{
-		CashBalance: cash_balance,
+		CashBalance: cashBalance,
 		ID:          farmer.ID,
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error adding cash owed to farmer database: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Error adding cash owed to farmer database: %v", err))
 		return
 	}
 
 	err = apiCfg.DB.MarkFarmerAsUpdated(r.Context(), farmer.ID)
 	if err != nil {
-		respondWithError(w, 500, fmt.Sprintf("Error updating farmer: %v", err))
+		jsonresponses.RespondWithError(w, 500, fmt.Sprintf("Error updating farmer: %v", err))
 	}
 
 	fmt.Printf("Farmer %v updated at updated to %v\n", farmer.Name, time.Now())
 
-	respondWithJSON(w, 201, purchase)
+	jsonresponses.RespondWithJSON(w, 201, purchase)
 }
 
-func (apiCfg *apiConfig) handlerGetPurchases(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *ApiConfig) HandlerGetPurchases(w http.ResponseWriter, r *http.Request) {
 	purchases, err := apiCfg.DB.GetPurchases(r.Context())
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't fetch purchases: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't fetch purchases: %v", err))
 		return
 	}
-	purchasesWithNames := []Purchase{}
-	purchasesToPublish := []Purchase{}
+	purchasesWithNames := []models.Purchase{}
+	purchasesToPublish := []models.Purchase{}
 
 	for _, purchase := range purchases {
-		purchasesWithNames = append(purchasesWithNames, databasePurchaseToPurchase(purchase))
+		purchasesWithNames = append(purchasesWithNames, models.DatabasePurchaseToPurchase(purchase))
 	}
 	for _, purchaseWithName := range purchasesWithNames {
 		user, err := apiCfg.DB.GetUserByID(r.Context(), purchaseWithName.UserID)
 		if err != nil {
-			respondWithError(w, 404, fmt.Sprintf("Couldn't fetch user: %v", err))
+			jsonresponses.RespondWithError(w, 404, fmt.Sprintf("Couldn't fetch user: %v", err))
 			return
 		}
 		purchaseWithName.UserName = user.Name
 		farmer, err := apiCfg.DB.GetFarmerByID(r.Context(), purchaseWithName.FarmerID)
 		if err != nil {
-			respondWithError(w, 404, fmt.Sprintf("Couldn't fetch farmer: %v", err))
+			jsonresponses.RespondWithError(w, 404, fmt.Sprintf("Couldn't fetch farmer: %v", err))
 			return
 		}
 		purchaseWithName.FarmerName = farmer.Name
@@ -142,10 +148,10 @@ func (apiCfg *apiConfig) handlerGetPurchases(w http.ResponseWriter, r *http.Requ
 
 		purchasesToPublish = append(purchasesToPublish, purchaseWithName)
 	}
-	respondWithJSON(w, 200, purchasesToPublish)
+	jsonresponses.RespondWithJSON(w, 200, purchasesToPublish)
 }
 
-func (apiCfg *apiConfig) handlerGetPurchaseByID(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *ApiConfig) HandlerGetPurchaseByID(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		ID uuid.UUID `json:"purchase_id"`
 	}
@@ -154,62 +160,62 @@ func (apiCfg *apiConfig) handlerGetPurchaseByID(w http.ResponseWriter, r *http.R
 	decode := json.NewDecoder(r.Body)
 	err := decode.Decode(&params)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't decode JSON: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't decode JSON: %v", err))
 		return
 	}
 
 	purchase, err := apiCfg.DB.GetPurchaseByID(r.Context(), params.ID)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't get purchase: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't get purchase: %v", err))
 	}
-	respondWithJSON(w, 200, purchase)
+	jsonresponses.RespondWithJSON(w, 200, purchase)
 }
 
-func (apiCfg *apiConfig) handlerDeletePurchase(w http.ResponseWriter, r *http.Request, user database.User) {
+func (apiCfg *ApiConfig) HandlerDeletePurchase(w http.ResponseWriter, r *http.Request, user database.User) {
 	purchaseIDStr := chi.URLParam(r, "purchase_id")
 	purchaseID, err := uuid.Parse(purchaseIDStr)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't parse purchase ID for deletion: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't parse purchase ID for deletion: %v", err))
 		return
 	}
 
 	purchase, err := apiCfg.DB.GetPurchaseByID(r.Context(), purchaseID)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't fetch purchase for deletion: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't fetch purchase for deletion: %v", err))
 		return
 	}
 
-	chicken_balance := sql.NullFloat64{}
-	chicken_balance.Float64 = float64(purchase.Chicken)
-	chicken_balance.Valid = true
+	chickenBalance := sql.NullFloat64{}
+	chickenBalance.Float64 = float64(purchase.Chicken)
+	chickenBalance.Valid = true
 
 	err = apiCfg.DB.DecreaseChickenOwed(r.Context(), database.DecreaseChickenOwedParams{
-		ChickenBalance: chicken_balance,
+		ChickenBalance: chickenBalance,
 		ID:             purchase.FarmerID,
 	})
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't decrease chicken owed: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't decrease chicken owed: %v", err))
 		return
 	}
 
-	cash_balance := sql.NullInt32{}
-	cash_balance.Int32 = purchase.Chicken * purchase.PricePerChicken
-	cash_balance.Valid = true
+	cashBalance := sql.NullInt32{}
+	cashBalance.Int32 = purchase.Chicken * purchase.PricePerChicken
+	cashBalance.Valid = true
 
 	err = apiCfg.DB.DecreaseCashOwed(r.Context(), database.DecreaseCashOwedParams{
-		CashBalance: cash_balance,
+		CashBalance: cashBalance,
 		ID:          purchase.FarmerID,
 	})
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't decrease cash owed: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't decrease cash owed: %v", err))
 		return
 	}
 
 	err = apiCfg.DB.DeletePurchase(r.Context(), purchaseID)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't parse purchase ID for deletion: %v", err))
+		jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't parse purchase ID for deletion: %v", err))
 		return
 	}
-	respondWithJSON(w, 200, fmt.Sprintf("Purchase successfully deleted by %v", user.Name))
+	jsonresponses.RespondWithJSON(w, 200, fmt.Sprintf("Purchase successfully deleted by %v", user.Name))
 
 }
