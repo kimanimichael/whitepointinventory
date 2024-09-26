571a6b725261cc6acafed70ec1f9c9b9f1b5ab1d
MK <muchunumike@gmail.com>
2024-09-26 08:24:22 +0300

    refactor(standard_layout): Get user from cookie

--- internal/api/httpapi/user_handler.go
+++ internal/api/httpapi/user_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 	"github.com/mike-kimani/fechronizo/v2/pkg/httpresponses"
 	"github.com/mike-kimani/whitepointinventory/internal/app"
 	"github.com/mike-kimani/whitepointinventory/pkg/auth"
@@ -28,6 +29,7 @@ func NewUserHandler(service app.UserService) *UserHandler {
 func (h *UserHandler) RegisterRoutes(router chi.Router) {
 	router.Post("/user", h.CreateUser)
 	router.Post("/login", h.UserLogin)
+	router.Get("/users", h.GetUserFromCookie)
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -91,5 +93,29 @@ func (h *UserHandler) UserLogin(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 	}
 	http.SetCookie(w, &cookie)
+	httpresponses.RespondWithJson(w, http.StatusOK, user)
+}
+
+func (h *UserHandler) GetUserFromCookie(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("jwt")
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error getting cookie: %v", err))
+	}
+	tokenString := cookie.Value
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error getting token: %v", err))
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+	userID, err := uuid.Parse(claims.Issuer)
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error parsing userID: %v", err))
+	}
+	user, err := h.service.GetUserByID(userID)
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error getting user: %v", err))
+	}
 	httpresponses.RespondWithJson(w, http.StatusOK, user)
 }
