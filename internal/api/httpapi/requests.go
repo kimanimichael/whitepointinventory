3b15b76efcd8c07142d9c7c4c55feb8d841d293a
MK <muchunumike@gmail.com>
2024-09-29 22:06:53 +0300

    refactor(standard_layout): Get farmers request struct

--- internal/api/httpapi/requests.go
+++ internal/api/httpapi/requests.go
@@ -11,3 +11,7 @@ type CreateFarmerRequest struct {
 	ChickenBalance float64 `json:"chicken_balance"`
 	CashBalance    int32   `json:"cash_balance"`
 }
+
+type GetFarmerRequest struct {
+	Name string `json:"name"`
+}
