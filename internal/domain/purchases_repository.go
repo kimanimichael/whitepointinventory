4267369962ec8f858714857c6313bcd10fc46c74
MK <muchunumike@gmail.com>
2024-09-30 22:19:33 +0300

    refactor(standard_layout): Remove unused parameter

--- internal/domain/purchases_repository.go
+++ internal/domain/purchases_repository.go
@@ -5,5 +5,5 @@ import "github.com/google/uuid"
 type PurchasesRepository interface {
 	CreatePurchase(chickenNo int32, chickenPrice int32, farmerName string, user *User) (*Purchase, error)
 	GetPurchaseByID(ID uuid.UUID) (*Purchase, error)
-	GetPurchases(ID uuid.UUID) ([]*Purchase, error)
+	GetPurchases() ([]Purchase, error)
 }
