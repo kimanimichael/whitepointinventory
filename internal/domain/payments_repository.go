581d03fa8d18dae74713031ecb8a6e281d675ca2
MK <muchunumike@gmail.com>
2024-10-02 00:18:24 +0300

    refactor(standard_layout): Define payments repository interface

---
+++ internal/domain/payments_repository.go
@@ -0,0 +1,10 @@
+package domain
+
+import "github.com/google/uuid"
+
+type PaymentsRepository interface {
+	CreatePayment(cashPaid, chickenPrice int32, farmerName string, user *User) (*Payment, error)
+	GetPaymentByID(ID uuid.UUID) (*Payment, error)
+	GetPayments() ([]Payment, error)
+	DeletePayment(ID uuid.UUID) error
+}
