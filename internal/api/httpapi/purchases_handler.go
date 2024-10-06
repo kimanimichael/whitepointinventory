da3bb695074022b51ff4276e7382b665922bac88
MK <muchunumike@gmail.com>
2024-10-06 11:06:00 +0300

    refactor(standard_layout): Implement get purchases handlers

--- internal/api/httpapi/purchases_handler.go
+++ internal/api/httpapi/purchases_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 	"github.com/mike-kimani/fechronizo/v2/pkg/httpresponses"
 	"github.com/mike-kimani/whitepointinventory/internal/app"
 	"github.com/mike-kimani/whitepointinventory/internal/domain"
@@ -53,4 +54,28 @@ func (h *PurchasesHandler) CreatePurchase(w http.ResponseWriter, r *http.Request
 		return
 	}
 	httpresponses.RespondWithJson(w, http.StatusOK, purchase)
+}
+
+func (h *PurchasesHandler) GetPurchaseByID(w http.ResponseWriter, r *http.Request, user *domain.User) {
+	purchaseIDStr := chi.URLParam(r, "purchase_id")
+	purchaseID, err := uuid.Parse(purchaseIDStr)
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Could not parse purchase ID %s: err %s", purchaseIDStr, err))
+		return
+	}
+	purchase, err := h.service.GetPurchaseByID(purchaseID)
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	httpresponses.RespondWithJson(w, http.StatusOK, purchase)
+}
+
+func (h *PurchasesHandler) GetPurchases(w http.ResponseWriter, r *http.Request, user *domain.User) {
+	purchases, err := h.service.GetPurchases()
+	if err != nil {
+		httpresponses.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	httpresponses.RespondWithJson(w, http.StatusOK, purchases)
 }
